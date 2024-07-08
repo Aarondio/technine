2a134be061a39cf8dd96e2ff22d519be44cca2f2
Aarondio <31071454+Aarondio@users.noreply.github.com>
2024-07-08 14:23:52 +0100

    Update organisationTest.php

--- tests/Unit/organisationTest.php
+++ tests/Unit/organisationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+
 use App\Models\Organisation;
 use App\Models\User;
 use Tests\TestCase;
@@ -14,7 +15,20 @@ class organisationTest extends TestCase
     /** @test */
     public function users_cannot_access_data_from_organizations_they_are_not_associated_with()
     {
-  
-      
+
+
+        $organizationA = Organisation::factory()->create(['name' => "Org A"]);
+        $organizationB = Organisation::factory()->create(['name' => "Org B"]);
+
+        $userA = User::factory()->create();
+        $userA->organisations()->attach($organizationA->id);
+
+        $userB = User::factory()->create();
+
+        $responseA = $this->actingAs($userA)->get('/api/organizations/');
+        $responseB = $this->actingAs($userA)->get('/api/organizations/');
+
+        $responseA->assertStatus(200);
+        $responseB->assertStatus(403);
     }
 }
